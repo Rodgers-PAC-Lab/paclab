3d2894884e7706b772940feb153414639c8cb321
Chris Rodgers <xrodgers@gmail.com>
2022-04-27 12:16:55 -0400

    fix imports

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 # This module is for shared lab scripts and functions
 # It should be importable like `import paclab`
+# Then the user can type paclab.paths.get_path_to_terminal_data()
+
+from . import paths
