46863b73347cc88955f1d1de9e6e1557a88a5842
Chris Rodgers <xrodgers@gmail.com>
2025-01-22 16:00:24 -0500

    doc and categorization

--- abr/signal_processing.py
+++ abr/signal_processing.py
@@ -5,7 +5,8 @@ import scipy.signal
 import numpy as np
 
 def identify_click_times(speaker_signal_V, threshold_V,
-    highpass_freq=50, sampling_rate=16000, refrac_samples=750):
+    highpass_freq=50, sampling_rate=16000, refrac_samples=750,
+    slice_start_sample=None, slice_stop_sample=None):
     """Identify when the clicks occurred
     
     speaker_signal : array
@@ -17,28 +18,135 @@ def identify_click_times(speaker_signal_V, threshold_V,
     refrac_samples : scalar
         Minimum time between clicks ("refractory period") in samples
     
-    Returns: speaker_signal_hp, peak_times
-        speaker_signal_hp : array
-            The speaker siganl highpassed
-        peak_times : array
+    slice_start_sample : int or None
+        If not None, should be an integer index indicating how much data
+        will be sliced before the click (example: -10). Peak times within
+        -slice_start_sample of the beginning of data will be dropped.
+        
+    slice_stop_sample : int or None
+        If not None, should be an integer index indicating how much data
+        will be sliced before the click (example: +10). Peak times within
+        -slice_stop_sample of the end of data will be dropped.
+    
+    Returns: dict with the following items
+        highpass : array
+            The speaker signal highpassed
+        peak_time_samples : array
             Peak times in samples
+        peak_properties : dict
     """
     ## Highpass audio data above 50 Hz to remove noise
     # This remove some baseline noise, but also makes the big clicks "ring"
     nyquist_freq = sampling_rate / 2.0
     ad_ahi, ad_bhi = scipy.signal.butter(
         2, (highpass_freq / nyquist_freq), btype='highpass')
-    speaker_signal_hp = scipy.signal.lfilter(ad_ahi, ad_bhi, speaker_signal_V)
+    
+    # Using lfilter ensures that the real peak always comes first, but
+    # also introduces a delay. 
+    # Using filtfilt removes the possibility of delay. 
+    # In practice, they both work fine, and we are no longer using "which
+    # came first" to choose peaks, but rather "which is bigger" (in find_peaks)
+    speaker_signal_hp = scipy.signal.filtfilt(ad_ahi, ad_bhi, speaker_signal_V)
 
-    # Find peaks
-    pos_peaks, pos_props = scipy.signal.find_peaks(
-        speaker_signal_hp, height=threshold_V, distance=refrac_samples)
+    # The problem with finding negative and positive peaks separately is that
+    # the refractory period ("distance") is only enforced within one or the
+    # other. Rather than enforce it twice, just run on the absolute value.
+    #
+    # height : peak must be at least this high
+    # threshold : this is a criterion on diff between adjacent samples, we
+    #   don't use this because it can depend on when the sample occurred
+    # distance : refractory period. Smaller peaks are removed first.
+    # prominence : minimum distance from height to the nearest valley within
+    #   `wlen` samples. Since we use abs(), prominence will always be less than
+    #   height, so we use a slightly more lenient criteria here. 
+    # width : (min, max) width of the peak. We know peaks should be very
+    #   narrow, no more than three samples. Due to interpolation, this is a float
+    # wlen : number of samples over which to seek for a valley in calculating
+    #   prominence. Since our peaks are narrow and we've highpassed, this 
+    #   can be pretty short.
+    # rel_height : how far the signal has to fall from peak in order to
+    #   calucate "width". The thresh is peak_height - rel_height * peak_prominence
+    peak_times, peak_props = scipy.signal.find_peaks(
+        np.abs(speaker_signal_hp), 
+        height=threshold_V, 
+        distance=refrac_samples,
+        prominence=threshold_V * .7,
+        width=(0.5, 3.5),
+        rel_height=0.5,
+        wlen=20,
+        )
 
-    neg_peaks, neg_props = scipy.signal.find_peaks(
-        -speaker_signal_hp, height=threshold_V, distance=refrac_samples)
+    # Drop peak_times within left_margin or right_margin of the edge
+    if slice_start_sample is not None:
+        peak_times = peak_times[peak_times > -slice_start_sample]
+    
+    if slice_stop_sample is not None:
+        peak_times = peak_times[
+            peak_times < len(speaker_signal_hp) - slice_stop_sample]
 
-    # Concatenate pos and neg peaks,
-    #  then drop ringing/overshoot peaks during refractory period
-    peak_times = np.sort(np.concatenate([pos_peaks, neg_peaks]))
+    return {
+        'highpassed': speaker_signal_hp,
+        'peak_time_samples': peak_times,
+        'peak_properties': peak_props,
+        }
 
-    return speaker_signal_hp, peak_times
+def categorize_clicks(click_times, speaker_signal_hp, amplitude_cuts, 
+    amplitude_labels):
+    """Categorize click times by amplitude
+    
+    click_times : array
+        Obtained from identify_click_times
+    
+    speaker_signal_hp : array
+        The highpass filtered speaker signal
+        This is used to pull the amplitude of each click at its click_time
+    
+    amplitude_cuts : array
+        The edges of the bins for each amplitude category
+        These must be in the units of log(abs(speaker_signal_hp))
+    
+    amplitude_labels : array
+        The labels to assign to each category
+        len(amplitude_labels) must be len(amplitude_cuts) - 1
+    
+    Returns: DataFrame
+        t_samples : time in samples (same as click_times)
+        amplitude_V : amplitude of the click in same units as speaker_signal_hp
+        amplitude_log_abs : abs(log10(amplitude_V))
+        amplitude_idx : the index into amplitude_labels for this click
+        polarity : True if click is positive else False
+        label : the category label for the click
+    """
+    ## Categorize the onsets
+    # Convert to series
+    click_params = pandas.Series(click_times, name='t_samples').to_frame()
+
+    # Extract the amplitude of each click
+    click_params['amplitude_V'] = speaker_signal_hp[click_times]
+    click_params['amplitude_log_abs'] = np.log10(
+        np.abs(click_params['amplitude_V']))
+
+    # Cut
+    click_params['amplitude_idx'] = pandas.cut(
+        click_params['amplitude_log_abs'], 
+        bins=amplitude_cuts, labels=False)
+
+    # Drop any that are above the last cut
+    big_click_mask = (
+        click_params['amplitude_log_abs'] > amplitude_cuts[-1])
+    if np.any(big_click_mask):
+        print('warning: dropping {} too large clicks'.format(
+            big_click_mask.sum()))
+        click_params = click_params[~big_click_mask]
+
+    # After this, there should be no clicks with null amplitude_idx
+    click_params['amplitude_idx'] = click_params['amplitude_idx'].astype(int)
+
+    # Label
+    click_params['label'] = amplitude_labels[
+        click_params['amplitude_idx']]
+
+    # Define polarity
+    click_params['polarity'] = click_params['amplitude_V'] > 0
+    
+    return click_params
