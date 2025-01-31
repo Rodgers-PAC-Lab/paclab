01feaebd0988b9d46fbcffbc7809061a08f054f6
PAC Lab <rodgers.paclab@gmail.com>
2025-01-31 17:11:03 -0500

    log to logfiles

--- abr/gui/start_gui.sh
+++ abr/gui/start_gui.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # This must be run with "bash -i" for conda to work
-
-conda activate py3 && python3 -m paclab.abr.gui.start_gui
+conda activate py3 && python3 -m paclab.abr.gui.start_gui |& tee -a /home/mouse/mnt/cuttlefish/surgery/abr_logfiles/logfile.$(date '+%F-%T-%N')
 
 echo ABR GUI closed. Press any key to close this window. 
 echo PAC Lab personnel - do not ignore this message\! Close this window\!
