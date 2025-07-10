b398a71dab0ba8f3c64c7b49f24e27932a3d4bb7
Rowan G <kgargiullo@gmail.com>
2025-07-10 17:18:40 -0400

    Added a function to plot a single ABR, grouped by sound level, on an axis.

--- abr/abr_plotting.py
+++ abr/abr_plotting.py
@@ -1,6 +1,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas
 
 
 def rint(arr):
@@ -17,3 +18,22 @@ def generate_colorbar(n_colors, mapname='jet', rounding=100, start=0.3, stop=1.)
     color_idxs = rint(rounding * np.linspace(start, stop, n_colors))[::-1]
     colors = plt.get_cmap(mapname, rounding)(color_idxs)
     return colors
+
+def plot_single_ax_abr(abr_subdf, ax, sampling_rate=16000):
+    """
+    PARAMETERS:
+        abr_subdf: a subdf where the index is sound levels and the columns are voltages
+        ax: the axis to plot it on
+        t: the x axis in ms
+    RETURNS:
+        ax: the axis object with the plot made
+    """
+    t = abr_subdf.columns/sampling_rate*1000
+    for label_i in abr_subdf.index.sort_values(ascending=False):
+        aut_colorbar = generate_colorbar(
+            len(abr_subdf.index), mapname='inferno_r', start=0.15, stop=1)
+        color_df = pandas.DataFrame(aut_colorbar,
+                                    index=abr_subdf.index.sort_values(ascending=True))
+        ax.plot(t, abr_subdf.loc[label_i].T * 1e6, lw=.75,
+                color=color_df.loc[label_i], label=label_i)
+    return ax
