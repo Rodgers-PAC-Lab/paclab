054c36007c3208251165b3ced43684833dd7a164
Chris Rodgers <xrodgers@gmail.com>
2025-11-21 10:19:33 -0500

    start deleting redundant code

--- abr/abr.py
+++ abr/abr.py
@@ -13,31 +13,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import my.plot
 
-def clear_all_lines_and_images_from_figure(fig):
-    """Remove all `lines` and `images` from all axes in `fig`"""
-    for ax in fig.axes:
-        clear_all_lines_and_images(ax)
-
-def clear_all_lines_and_images(ax):
-    """Remove all `lines` and `images` from `ax`"""
-    while len(ax.lines) > 0:
-        ax.lines[0].remove()
-
-    while len(ax.images) > 0:
-        ax.images[0].remove()
-
-def move_figure(f, x, y):
-    """Move figure's upper left corner to pixel (x, y)"""
-    backend = matplotlib.get_backend()
-    if backend == 'TkAgg':
-        f.canvas.manager.window.wm_geometry("+%d+%d" % (x, y))
-    elif backend == 'WXAgg':
-        f.canvas.manager.window.SetPosition((x, y))
-    else:
-        # This works for QT and GTK
-        # You can also use window.setGeometry
-        f.canvas.manager.window.move(x, y)
-
+# Three versions of this one - here, abr_gui.py, loading.py
 def get_metadata(notes_directory, data_directory, datestring,
                  metadata_version, day_directory = '_ABR'):
     """Get the metadata from a day of recordings
@@ -110,93 +86,6 @@ def get_metadata(notes_directory, data_directory, datestring,
 
     return metadata
 
-def parse_header(header):
-    """Parse data from a single header
-
-    `header`: 60 bytes
-
-    Returns: dict, with the following items:
-        packet_number : number of packet within the file
-        number_channels : typically 8
-        number_samples : this is the number of samples per packet
-        trigger_position : whether a trigger occurred in this packet
-            -1 if no trigger
-            Otherwise, an index into the packet
-        header_ver : typically 2
-        total_pkts : total number of packets in the file
-            This is only valid if it's the first header and if Labview
-            completed the recording. Otherwise it's zero.
-        sampling_sps : sampling rate
-        gains : an array of channel gains
-    """
-    res = {}
-
-    res['packet_number'] = struct.unpack('<I', header[0:4])[0]
-    res['number_channels'] = struct.unpack('<I', header[4:8])[0]
-    res['number_samples'] = struct.unpack('<I', header[8:12])[0]
-    res['trigger_position'] = struct.unpack('<i', header[12:16])[0]
-    res['header_ver'] = struct.unpack('<i', header[16:20])[0]
-    res['total_pkts'] = struct.unpack('<I', header[20:24])[0]
-    res['sampling_sps'] = struct.unpack('<i', header[24:28])[0]
-    res['gains'] = np.array(struct.unpack('<8i', header[28:60]))
-
-    return res
-
-def parse_data(data_bytes, number_channels, number_samples):
-    """Parse bytes into acquired data
-
-    Takes the `data_bytes` that have been read from disk, removes all
-    the header information, converts to float, concatenates across packets,
-    and returns.
-
-    data_bytes : bytes
-        This is the data read from disk
-
-    Returns: array of shape (n_samples, n_channels)
-        This is all the data from each channel.
-    """
-    # Each packet comprises a 60 byte headers (15 ints) and a 16000 byte data
-    # payload (4000 floats)
-
-    # It's easiest to convert everything to float, and then just dump the
-    # floats that correspond to the headers
-
-    # Unpack it all to floats
-    n_floats = len(data_bytes) // 4
-
-    # The old, slow way
-    # data = np.array(struct.unpack('<{}f'.format(n_floats), data_bytes))
-
-    # This is faster
-    # https://stackoverflow.com/questions/36797088/speed-up-pythons-struct-unpack
-    data = np.ndarray(
-        (n_floats,), dtype=np.float32, buffer=data_bytes).astype(float)
-
-    # Truncate to a multiple of 4015
-    total_packet_size = 15 + number_channels * number_samples
-    n_complete_packets = len(data) // total_packet_size
-    if len(data) / total_packet_size != n_complete_packets:
-        print("warning: data length was not a multiple of packet length")
-        data = data[:total_packet_size * n_complete_packets]
-
-    # Reshape into packets -- one packet (4015 bytes) per row
-    data = data.reshape(-1, total_packet_size)
-
-    # Drop the first 15 bytes of each row, which are the headers
-    # TODO: instead of dropping, convert to ints, and extract header
-    # of each packet
-    data = data[:, 15:]
-
-    # Each row needs to be reshaped into (number_channels, number_samples)
-    # Note that the data is structured as sample-first;channel-second
-    # which doesn't really make sense
-    data = data.reshape((-1, number_channels, number_samples))
-
-    # Concatenate all packets together (axis=1 accounts for the sample-first
-    # problem). Transpose so each channel is a column
-    data = np.concatenate(data, axis=1).T
-
-    return data
 
 def extract_single_ad_and_nrl(data,neural_channel, speaker_channel, audio_drop_threshold, neural_drop_threshold):
     """
