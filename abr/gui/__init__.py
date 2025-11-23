a48b7a1213d9dddb992d155e5363b55dfe7c8e58
Chris Rodgers <xrodgers@gmail.com>
2025-11-22 19:42:01 -0500

    remove all abr files

--- abr/gui/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import ABR_Device
-from . import graphics
-from . import serial_comm
-from . import serial_io
-from . import file_io
-from . import main_window
--- abr/gui/start_gui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This must be run with "bash -i" for conda to work
-
-# -u makes python run in unbuffered mode so we can see error messages
-conda activate py3 && python3 -u -m paclab.abr.gui.start_gui |& tee -a /home/mouse/mnt/cuttlefish/surgery/abr_logfiles/logfile.$(date '+%F-%T-%N')
-
-echo ABR GUI closed. Press any key to close this window. 
-echo PAC Lab personnel - do not ignore this message\! Close this window\!
-read
