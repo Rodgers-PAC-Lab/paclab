6cc971b6d8c89fa1795ac2d46481bd6515391067
Chris Rodgers <xrodgers@gmail.com>
2025-11-26 15:40:27 -0500

    distributing functions from the old abr subdirectory to where they belong

--- misc.py
+++ misc.py
@@ -2,6 +2,22 @@
 
 import matplotlib.mlab
 
+def rint(arr):
+    """Round with rint and cast to int
+
+    If `arr` contains NaN, casting it to int causes a spuriously negative
+    number, because NaN cannot be an int. In this case we raise ValueError.
+    """
+    if np.any(np.isnan(np.asarray(arr))):
+        raise ValueError("cannot convert arrays containing NaN to int")
+    return np.rint(arr).astype(int)
+
+def generate_colorbar(n_colors, mapname='jet', rounding=100, start=0.3, stop=1.):
+    """Generate N evenly spaced colors from start to stop in map"""
+    color_idxs = rint(rounding * np.linspace(start, stop, n_colors))[::-1]
+    colors = plt.get_cmap(mapname, rounding)(color_idxs)
+    return colors
+    
 def psd(data, NFFT=None, Fs=None, detrend='mean', window=None, noverlap=None,
     scale_by_freq=None, **kwargs):
     """Compute power spectral density.
